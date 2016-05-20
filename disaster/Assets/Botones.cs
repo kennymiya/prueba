d0d8e811fd24d22ec5a247f5622247965032982f
kenny miyasato <kenny miyasato>
2016-05-20 14:03:54 -0500

    subo el archivo de prueba

---
+++ disaster/Assets/Botones.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class Botones : MonoBehaviour {
+
+	Button startss;
+	Button option;
+	Button credits;
+
+	void Start () {
+	
+		startss=GetComponent<Button> ();
+		option =GetComponent<Button> ();
+		credits =GetComponent<Button> ();
+		//back = GameObject.Find ("back").GetComponent<Button> ();
+		cambios ();
+
+
+	}
+	void cambios(){
+		switch (gameObject.name) {
+		case "start":
+			
+			startss.onClick.AddListener (() => goGame ());
+			break;
+		case "options":
+			option.onClick.AddListener (() => goOptions ());
+			break;
+		case "credits":
+			credits.onClick.AddListener (() => goCredits ());
+			break;
+	
+		}
+		
+	}
+
+	void goGame(){
+		
+		SceneManager.LoadScene ("Game");
+	
+	}
+	void goOptions(){
+
+		SceneManager.LoadScene ("Options");
+	
+
+	}
+	void goCredits(){
+
+		SceneManager.LoadScene ("Credits");
+
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ disaster/Assets/music.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class music : MonoBehaviour {
+
+	AudioSource musica;
+	public static music instance;
+	void Awake(){
+		instance = this;
+		DontDestroyOnLoad (gameObject);
+	}
+	void Start(){
+		playMusic ();
+	}
+	public void playMusic () {
+		musica = GameObject.Find("audiosourceMusica").GetComponent<AudioSource>();
+		musica.Play ();
+		PlayerPrefs.SetFloat ("volumenambiente", musica.volume);
+	}
+	public void stopMusic(){
+		musica.Stop ();
+	}
+
+}
